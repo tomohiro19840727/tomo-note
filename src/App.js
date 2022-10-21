258dbc026b2d07012af78d15172993f7f87d02b6
tomohiro <tomohirofarm@gmail.com>
2022-10-21 14:08:29 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import Main from './components/Main';
+import Sidebar from './components/Sidebar';
+import uuid from "react-uuid";
 
 function App() {
+  const [notes, setNotes] = useState(JSON.parse(localStorage.getItem("notes")) || []);
+  const [activeNote, setActiveNote] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes))
+  },[notes])
+
+  useEffect(() => {
+   setActiveNote(notes[0].id);
+  },[]);
+
+  const onAddNote = () => {
+    console.log("追加されました");
+    const newNote = {
+      id: uuid(),
+      title: "新しいノート",
+      content: "新しいノートの内容",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote])
+    console.log(notes);
+  }
+
+  const onDeleteNote = (id) => {
+    const filterNotes = notes.filter((note) => note.id !== id);
+    setNotes(filterNotes);
+  }
+
+  const getActiveNote = () => {
+    return notes.find((note) => note.id === activeNote)
+  }
+
+  const onUpdateNote = (updatedNote) => {
+    // 修正された新しいノートの配列を返す。
+    const updateNotesArray = notes.map((note) => {
+      if(note.id === updatedNote.id) {
+        // console.log(updatedNote); つまり書き換えたもの
+        return updatedNote;
+      } else {
+         return note;
+          // そのまま変わらないもの
+      };
+    });
+    // console.log(updateNotesArray); これは配列としてサイドに出ている
+    setNotes(updateNotesArray);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Sidebar  onAddNote={onAddNote} notes={notes} onDeleteNote={onDeleteNote}  activeNote={activeNote} setActiveNote={setActiveNote} />
+      <Main  activeNote={getActiveNote()}  onUpdateNote={onUpdateNote} />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./Main.css";
+
+const Main = ({ activeNote , onUpdateNote }) => {
+  const onEditNote = (key, value) => {
+   onUpdateNote({
+    ...activeNote,
+    //  スプレッド構文で付け足していく
+    [key]: value,
+    //  動的キーで下のキーによって変わるもの valueは文字
+    modDate: Date.now(),
+   });
+  };
+
+  if(!activeNote) {
+    return <div className='no-active-note'>ノートが選択されていません</div>
+  }
+
+  return (
+    <div className='app-main'>
+      <div className='app-main-note-edit'>
+        <input  id="title" type="text" value={activeNote.title}  onChange={(e) => onEditNote("title", e.target.value)} />
+        <textarea id="content" placeholder="ノート内容を記入"  value={activeNote.content} onChange={(e) => onEditNote("content", e.target.value)}></textarea>
+      </div>
+      <div className='app-main-note-preview'>
+        <h1 className='preview-title'>{activeNote.title}</h1>
+        <div className='markdown-preview'>{activeNote.content}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Main
